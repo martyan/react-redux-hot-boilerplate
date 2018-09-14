12196f91e68b533d251270d39896cb0f5c87673a
Martin Juzl <martin.juzl@gmail.com>
2018-09-14 17:16:18 +0200

    sentry-ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import Raven from 'raven-js'
 import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader' /* react-hot-loader v3 */
 import { ConnectedRouter } from 'connected-react-router'
@@ -10,6 +11,10 @@ import LocalizationProvider from './containers/Localization/LocalizationProvider
 
 const history = createBrowserHistory()
 const store = configureStore(history)
+
+if(process.env.NODE_ENV === 'production') {
+//   Raven.config('<YOUR-SENTRY-ID>').install()
+}
 
 const render = () => {
   ReactDOM.render(
