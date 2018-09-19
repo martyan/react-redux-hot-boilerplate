45febcdc24b5c835013f0ebb9e5ffdd1a4ec68ec
Martin Juzl <martin.juzl@gmail.com>
2018-09-19 11:57:52 +0200

    styles loader

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-// import './App.scss'
+import './App.scss'
 
 const App = () => (
-	<div>polibte mi prdele</div>
+	<div styleName="polib">polibte mi prdele</div>
 )
 
 export default App
