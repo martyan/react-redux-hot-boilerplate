63cb9e2fdf7fa9641f43ad998a3366692ac4a47b
martyan <martin.juzl@gmail.com>
2019-03-13 19:44:49 +0100

    code style

--- src/server/template.js
+++ src/server/template.js
@@ -1,5 +1,6 @@
 export default ({ markup, helmet, reduxState }) => {
-    return `<!DOCTYPE html>
+    return `
+        <!DOCTYPE html>
 			<html ${helmet.htmlAttributes.toString()}>
 				<head>
 					${helmet.title.toString()}
@@ -8,9 +9,7 @@ export default ({ markup, helmet, reduxState }) => {
 				</head>
 				<body ${helmet.bodyAttributes.toString()}>
 					<div id="app">${markup}</div>
-					<script>
-            window.__INITIAL_STATE__ = ${JSON.stringify(reduxState)}
-	        </script>
+					<script>window.__INITIAL_STATE__ = ${JSON.stringify(reduxState)}</script>
 					<script src="/public/client.js" async></script>
 					<link href="/public/styles.css" rel="stylesheet" />
 				</body>
