61a792e72a95e1223dc23f79e1a674fa774234d7
martyan <martin.juzl@gmail.com>
2019-03-14 12:58:20 +0100

    wip

--- src/client/index.js
+++ src/client/index.js
@@ -3,24 +3,29 @@ import ReactDOM from 'react-dom'
 // import Raven from 'raven-js'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
+import Loadable from 'react-loadable'
 import App from './containers/App'
 import LocalizationProvider from './containers/Localization/LocalizationProvider'
 import configureStore from '../common/store'
 import history from '../common/history'
 
-const store = configureStore(history, window.__INITIAL_STATE__)
+window.main = () => {
+    Loadable.preloadReady().then(() => {
+        const store = configureStore(history, window.__INITIAL_STATE__)
 
-if(process.env.NODE_ENV === 'production') {
-//   Raven.config('<YOUR-SENTRY-ID>').install()
+        if(process.env.NODE_ENV === 'production') {
+            //Raven.config('<YOUR-SENTRY-ID>').install()
+        }
+
+        ReactDOM.hydrate(
+            <Provider store={store}>
+                <LocalizationProvider store={store}>
+                    <ConnectedRouter history={history}>
+                        <App/>
+                    </ConnectedRouter>
+                </LocalizationProvider>
+            </Provider>,
+            document.getElementById('app')
+        )
+    })
 }
-
-ReactDOM.hydrate(
-    <Provider store={store}>
-        <LocalizationProvider store={store}>
-            <ConnectedRouter history={history}>
-                <App/>
-            </ConnectedRouter>
-        </LocalizationProvider>
-    </Provider>,
-    document.getElementById('app')
-)
